da788a7d7010370dfc2f33576b817f0a25bf2a04
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-22 08:38:32 +0100

    Added fetch functionality, loading and error states

--- product.js
+++ product.js
@@ -3,7 +3,15 @@ const url =
   "https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog";
 
 const fetchProduct = async () => {
-  return "product";
+  try {
+    productDOM.innerHTML = '<h4 class="product-loading">Loading...</h4>';
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    productDOM.innerHTML =
+      '<p class="error">There was a problem loading the product. Please try again later.</h4>';
+  }
 };
 
 const displayProduct = (product) => {
