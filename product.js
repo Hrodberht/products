d17d048ee9e78b2373441018d751fd89d9064e8f
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-22 08:50:10 +0100

    Removed fetchProduct console log

--- product.js
+++ product.js
@@ -6,7 +6,6 @@ const fetchProduct = async () => {
     productDOM.innerHTML = '<h4 class="product-loading">Loading...</h4>';
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
-    console.log(id);
     const response = await fetch(`${url}?id=${id}`);
     const data = await response.json();
     return data;
