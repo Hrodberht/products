4506448ed54b5aba83fa22f7081ea934842d8bc3
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-22 08:49:01 +0100

    Added dynamic product display

--- product.js
+++ product.js
@@ -17,7 +17,33 @@ const fetchProduct = async () => {
 };
 
 const displayProduct = (product) => {
-  console.log(product);
+  const {
+    company,
+    colors,
+    description,
+    name: title,
+    price,
+    image,
+  } = product.fields;
+  const { url: img } = image[0];
+
+  document.title = title.toUpperCase();
+
+  productDOM.innerHTML = `<div class="product-wrapper">
+        <img src="${img}" class="img" alt="${title}" />
+        <div class="product-info">
+          <h3>${title}</h3>
+          <h5>${company}</h5>
+          <span>${price / 100}</span>
+          <div class="colors">
+            <span class="product-color"></span>
+          </div>
+          <p>
+            ${description}
+          </p>
+          <button class="btn">Add To Cart</button>
+        </div>
+      </div>`;
 };
 
 const start = async () => {
